e179d458e74b9edec94ee005b208407985967931
Sergio Camargo <sergiochidalgo@gmail.com>
2017-04-04 16:25:23 +0000

    return preferred language

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 var express = require('express');
+var parseAcceptLanguage = require('parse-accept-language');
 
 var app = express();
 var result = {};
 
 app.get('/',function(req,res){
-    result['ipaddress'] = "some-ip";
-    result['language'] = 'some-lang';
+    result['ipaddress'] = req.ip;
+    //result['ipaddress'] = req.connection.remoteAddress;
+    result['language'] = parseAcceptLanguage(req)[0].value;
     result['software'] = 'some-software';
     res.send(result);
 });
 
 app.listen(8080, function(){
-    console.log("listening on 8080")
+    console.log("listening on 8080");
 });
