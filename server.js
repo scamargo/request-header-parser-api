be9170469fffbc441a316fb5940d095f7e83cebb
Sergio Camargo <sergiochidalgo@gmail.com>
2017-04-04 16:59:51 +0000

    include ip address in response

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ var app = express();
 var result = {};
 
 app.get('/',function(req,res){
-    result['ipaddress'] = req.ip;
-    //result['ipaddress'] = req.connection.remoteAddress;
+
+    result['ipaddress'] = getClientIP(req);
+
     result['language'] = parseAcceptLanguage(req)[0].value;
+    
     var regExp = new RegExp(/\(([^)]+)\)/)
     result['software'] = regExp.exec(req.get("User-Agent"))[1];
+    
     res.send(result);
 });
+
+function getClientIP(req) {
+    var ip = req.get("X-Forwarded-For");
+    if(!ip) {
+        ip = req.ip;
+    }
+    return ip;
+}
 
 app.listen(8080, function(){
     console.log("listening on 8080");
